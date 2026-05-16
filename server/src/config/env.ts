372a3932c7d5039106b45d13ae5c5043b17f9ab5
dcpro8 <dhruvchauhanv121@gmail.com>
2026-05-16 09:27:06 +0530

    updated env.ts to accomodate env variable name change.

--- server/src/config/env.ts
+++ server/src/config/env.ts
@@ -5,6 +5,6 @@ export const env = {
   PORT: process.env.PORT || 5000,
   CLIENT_URL: process.env.CLIENT_URL || "*",
 
-  MINIMAX_API_KEY: process.env.MINIMAX_API_KEY || "",
-  MINIMAX_BASE_URL: process.env.MINIMAX_BASE_URL || "",
+  GROQ_API_KEY: process.env.GROQ_API_KEY || "",
+  GROQ_BASE_URL: process.env.GROQ_BASE_URL || "https://api.groq.com/openai/v1",
 };
